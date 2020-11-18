2140a3988d940a9532ce819e0fa633f263a84679
Juan Carlos Corrales Guerrero <jc.corrales@uniandes.edu.co>
2020-11-18 02:58:08 +0000

    First commit

---
+++ webapp/controller/VistaHojasDeGasto.controller.js
@@ -0,0 +1,251 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function (BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.maestroDetalle.controller.VistaHojasDeGasto", {
+		handleRouteMatched: function (oEvent) {
+			var sAppId = "App5f8b780311843c3c09294816";
+
+			var oParams = {};
+			var oView = this.getView();
+			var bSelectFirstListItem = true;
+			if (oEvent.mParameters.data.context || oEvent.mParameters.data.masterContext) {
+				this.sContext = oEvent.mParameters.data.context;
+
+				this.sMasterContext = oEvent.mParameters.data.masterContext;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function (oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype" && prop.includes("Set")) {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sMasterContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sMasterContext && oEvent.getParameters().config.bypassed.target[0] !== this.sMasterContext) {
+				oPath = {
+					path: "/" + this.sMasterContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			} else if (this.sContext) {
+				var sCurrentContextPath = "/" + this.sContext;
+
+				bSelectFirstListItem = false;
+			}
+
+			if (bSelectFirstListItem) {
+				oView.addEventDelegate({
+					onBeforeShow: function () {
+						var oContent = this.getView().getContent();
+						if (oContent) {
+							if (!sap.ui.Device.system.phone) {
+								var oList = oContent[0].getContent() ? oContent[0].getContent()[0] : undefined;
+								if (oList) {
+									var sContentName = oList.getMetadata().getName();
+									if (sContentName.indexOf("List") > -1) {
+										oList.attachEventOnce("updateFinished", function () {
+											var oFirstListItem = this.getItems()[0];
+											if (oFirstListItem) {
+												oList.setSelectedItem(oFirstListItem);
+												oList.fireItemPress({
+													listItem: oFirstListItem
+												});
+											}
+										}.bind(oList));
+									}
+								}
+							}
+						}
+					}.bind(this)
+				});
+			}
+
+		},
+		_attachSelectListItemWithContextPath: function (sContextPath) {
+			var oView = this.getView();
+			var oContent = this.getView().getContent();
+			if (oContent) {
+				if (!sap.ui.Device.system.phone) {
+					var oList = oContent[0].getContent() ? oContent[0].getContent()[0] : undefined;
+					if (oList && sContextPath) {
+						var sContentName = oList.getMetadata().getName();
+						var oItemToSelect, oItem, oContext, aItems, i;
+						if (sContentName.indexOf("List") > -1) {
+							if (oList.getItems().length) {
+								oItemToSelect = null;
+								aItems = oList.getItems();
+								for (i = 0; i < aItems.length; i++) {
+									oItem = aItems[i];
+									oContext = oItem.getBindingContext();
+									if (oContext && oContext.getPath() === sContextPath) {
+										oItemToSelect = oItem;
+									}
+								}
+								if (oItemToSelect) {
+									oList.setSelectedItem(oItemToSelect);
+								}
+							} else {
+								oView.addEventDelegate({
+									onBeforeShow: function () {
+										oList.attachEventOnce("updateFinished", function () {
+											oItemToSelect = null;
+											aItems = oList.getItems();
+											for (i = 0; i < aItems.length; i++) {
+												oItem = aItems[i];
+												oContext = oItem.getBindingContext();
+												if (oContext && oContext.getPath() === sContextPath) {
+													oItemToSelect = oItem;
+												}
+											}
+											if (oItemToSelect) {
+												oList.setSelectedItem(oItemToSelect);
+											}
+										});
+									}
+								});
+							}
+						}
+
+					}
+				}
+			}
+
+		},
+		updateBindingOptions: function (sCollectionId, oBindingData, sSourceId) {
+			this.mBindingOptions = this.mBindingOptions || {};
+			this.mBindingOptions[sCollectionId] = this.mBindingOptions[sCollectionId] || {};
+
+			var aSorters = this.mBindingOptions[sCollectionId].sorters;
+			var aGroupby = this.mBindingOptions[sCollectionId].groupby;
+
+			// If there is no oBindingData parameter, we just need the processed filters and sorters from this function
+			if (oBindingData) {
+				if (oBindingData.sorters) {
+					aSorters = oBindingData.sorters;
+				}
+				if (oBindingData.groupby || oBindingData.groupby === null) {
+					aGroupby = oBindingData.groupby;
+				}
+				// 1) Update the filters map for the given collection and source
+				this.mBindingOptions[sCollectionId].sorters = aSorters;
+				this.mBindingOptions[sCollectionId].groupby = aGroupby;
+				this.mBindingOptions[sCollectionId].filters = this.mBindingOptions[sCollectionId].filters || {};
+				this.mBindingOptions[sCollectionId].filters[sSourceId] = oBindingData.filters || [];
+			}
+
+			// 2) Reapply all the filters and sorters
+			var aFilters = [];
+			for (var key in this.mBindingOptions[sCollectionId].filters) {
+				aFilters = aFilters.concat(this.mBindingOptions[sCollectionId].filters[key]);
+			}
+
+			// Add the groupby first in the sorters array
+			if (aGroupby) {
+				aSorters = aSorters ? aGroupby.concat(aSorters) : aGroupby;
+			}
+
+			var aFinalFilters = aFilters.length > 0 ? [new sap.ui.model.Filter(aFilters, true)] : undefined;
+			return {
+				filters: aFinalFilters,
+				sorters: aSorters
+			};
+
+		},
+		createFiltersAndSorters: function () {
+			this.mBindingOptions = {};
+			var oBindingData, aPropertyFilters;
+			oBindingData = {};
+			oBindingData.filters = [];
+			aPropertyFilters = [];
+
+			aPropertyFilters.push(new sap.ui.model.Filter("FechaProcedimiento", "EQ", "2020-10-11"));
+			oBindingData.filters.push(new sap.ui.model.Filter(aPropertyFilters, true));
+
+			oBindingData.sorters = [];
+
+			oBindingData.sorters.push(new sap.ui.model.Sorter("Paciente", false, false));
+			this.updateBindingOptions("sap_List_Page_0-content-sap_m_ObjectList-1602981970616", oBindingData);
+			oBindingData = {};
+			oBindingData.filters = [];
+			aPropertyFilters = [];
+
+			aPropertyFilters.push(new sap.ui.model.Filter("FechaProcedimiento", "EQ", "2020-10-12"));
+			oBindingData.filters.push(new sap.ui.model.Filter(aPropertyFilters, true));
+
+			this.updateBindingOptions("sap_List_Page_0-content-sap_m_ObjectList-1602981975060", oBindingData);
+
+		},
+		applyFiltersAndSorters: function (sControlId, sAggregationName, chartBindingInfo) {
+			if (chartBindingInfo) {
+				var oBindingInfo = chartBindingInfo;
+			} else {
+				var oBindingInfo = this.getView().byId(sControlId).getBindingInfo(sAggregationName);
+			}
+			var oBindingOptions = this.updateBindingOptions(sControlId);
+			this.getView().byId(sControlId).bindAggregation(sAggregationName, {
+				model: oBindingInfo.model,
+				path: oBindingInfo.path,
+				parameters: oBindingInfo.parameters,
+				template: oBindingInfo.template,
+				templateShareable: true,
+				sorter: oBindingOptions.sorters,
+				filters: oBindingOptions.filters
+			});
+
+		},
+		onInit: function () {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("VistaHojasDeGasto").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+			this.mAggregationBindingOptions = {};
+			this.createFiltersAndSorters();
+
+			this.applyFiltersAndSorters("sap_List_Page_0-content-sap_m_ObjectList-1602981970616", "items");
+
+			this.applyFiltersAndSorters("sap_List_Page_0-content-sap_m_ObjectList-1602981975060", "items");
+
+		},
+		onExit: function () {
+
+			// to destroy templates for bound aggregations when templateShareable is true on exit to prevent duplicateId issue
+			var aControls = [{
+				"controlId": "sap_List_Page_0-content-sap_m_ObjectList-1602981970616",
+				"groups": ["items"]
+			}, {
+				"controlId": "sap_List_Page_0-content-sap_m_ObjectList-1602981975060",
+				"groups": ["items"]
+			}, {
+				"controlId": "sap_List_Page_0-content-sap_m_ObjectList-1",
+				"groups": ["items"]
+			}];
+			for (var i = 0; i < aControls.length; i++) {
+				var oControl = this.getView().byId(aControls[i].controlId);
+				if (oControl) {
+					for (var j = 0; j < aControls[i].groups.length; j++) {
+						var sAggregationName = aControls[i].groups[j];
+						var oBindingInfo = oControl.getBindingInfo(sAggregationName);
+						if (oBindingInfo) {
+							var oTemplate = oBindingInfo.template;
+							oTemplate.destroy();
+						}
+					}
+				}
+			}
+
+		}
+	});
+}, /* bExport= */ true);
